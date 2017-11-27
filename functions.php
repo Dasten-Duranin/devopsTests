98b055051da4f9dd86eba0ae39801a2b12d35821
Tanguy Le Roux <leroux.tanguy.51@gmail.com>
2017-11-27 15:45:42 +0100

    Correction

--- functions.php
+++ functions.php
@@ -9,9 +9,15 @@ class Functions
         return ob_get_clean();
     }
 
+    public static function getSuccessTemplate()
+    {
+        ob_start();
+        include('./success.php');
+        return ob_get_clean();
+    }
+
     public function valid()
     {
-        $valid = false;
         $email       = preg_match ( " /^[^\W][a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}$/ " , $_POST['email'] );
         $passwordLen = strlen($_POST['mdp']) >= 5;
         $passwordCap = preg_match("/[A-Z]/", $_POST['mdp']);
@@ -30,10 +36,9 @@ class Functions
         }
 
         if ($email && $passwordCap && $passwordLen) {
-            $valid = true;
-            header('/success.php');
+            echo self::getSuccessTemplate();
         } else {
-            self::getFormTemplate($passwordErr, $emailErr);
+            echo self::getFormTemplate($passwordErr, $emailErr);
         }
     }
 }
